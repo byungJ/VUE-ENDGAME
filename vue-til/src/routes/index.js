2d45da415707af087a523f65d40426e573d5ea9c
byungJ <qudwns6122@naver.com>
2022-03-13 13:49:33 +0900

    routes

--- vue-til/src/routes/index.js
+++ vue-til/src/routes/index.js
@@ -1,6 +1,29 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+//import LoginPage from '@/views/LoginPage.vue';
+//import SignupPage from '@/views/SignupPage.vue';
 
 Vue.use(VueRouter);
 
-export default new VueRouter();
+export default new VueRouter({
+  mode: 'history',
+  // vue router에 의해서 제어되는 페이지 정보를 담는 배열
+  routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      component: () => import('@/views/LoginPage.vue'),
+    },
+    {
+      path: '/signup',
+      component: () => import('@/views/SignupPage.vue'),
+    },
+    {
+      path: '*',
+      component: () => import('@/views/NotFoundPage.vue'),
+    },
+  ],
+});
