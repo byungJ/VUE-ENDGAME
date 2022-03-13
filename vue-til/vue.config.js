62b0bd8d1fa67cffa18fed029c768e7aaf7d90f3
byungJ <qudwns6122@naver.com>
2022-03-13 12:56:37 +0900

    2

--- vue-til/vue.config.js
+++ vue-til/vue.config.js
@@ -1,10 +1,5 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  //lintOnSave: false,
-  devServer: {
-    client: {
-      overlay: false,
-    },
-  },
-});
+module.exports = {
+	devServer: {
+		overlay: false,
+	},
+};
