d210f283b1827310407fdd3fd1351436e107ec51
byungJ <qudwns6122@naver.com>
2022-04-04 14:55:58 +0900

    router

--- vue-til/src/routes/index.js
+++ vue-til/src/routes/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '@/store/index.js';
 //import LoginPage from '@/views/LoginPage.vue';
 //import SignupPage from '@/views/SignupPage.vue';
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
   mode: 'history',
   // vue router에 의해서 제어되는 페이지 정보를 담는 배열
   routes: [
@@ -24,14 +25,23 @@ export default new VueRouter({
     {
       path: '/main',
       component: () => import('@/views/MainPage.vue'),
+      meta: {
+        auth: true,
+      },
     },
     {
       path: '/add',
       component: () => import('@/views/PostAddPage.vue'),
+      meta: {
+        auth: true,
+      },
     },
     {
       path: '/post/:id',
       component: () => import('@/views/PostEditPage.vue'),
+      meta: {
+        auth: true,
+      },
     },
     {
       path: '*',
@@ -39,3 +49,14 @@ export default new VueRouter({
     },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.auth && !store.getters.isLogin) {
+    next('/login');
+    // 다음 next()가 실행 되지 않습니다.
+    return;
+  }
+  next();
+});
+
+export default router;
